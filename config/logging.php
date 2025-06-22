dc4e4590a2f98721be48ccdba12469765920d65c
Juma <juma@Jumas-MacBook-Pro.local>
2025-06-22 21:15:07 +0300

    fixes

--- config/logging.php
+++ config/logging.php
@@ -45,6 +45,7 @@ return [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
+            'permission' => 0666,
         ],
 
         'daily' => [
@@ -52,6 +53,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
+            'permission' => 0666,
         ],
 
         'slack' => [
@@ -98,6 +100,7 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+            'permission' => 0666,
         ],
     ],
 
